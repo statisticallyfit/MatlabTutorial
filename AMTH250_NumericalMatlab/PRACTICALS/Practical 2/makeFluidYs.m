bd10bb77a86ae32b9aebc9a0a35df0e48f6b0363
BabitzdaBrave <amg.vintila@gmail.com>
2018-03-10 02:45:11 -0500

    working on fluids question 5

---
+++ AMTH250_NumericalMatlab/PRACTICALS/Practical 2/makeFluidYs.m
@@ -0,0 +1,13 @@
+function [xs, ys] = makeFluidYs(xs, force, constants)
+% Returns y-values, after passing the perturbation formula y
+% the force, and fluidData constants for a fluid. 
+    a = constants(1);
+    b = constants(2);
+    
+    % This is the fluid formula!
+    y = @(x, F, a, b) F * exp(-a*x) .* sin(b*x);
+    
+    % Make the y values
+    ys = y(xs, force, a, b)
+    
+end
