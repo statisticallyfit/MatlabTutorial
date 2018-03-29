7054267b7a2e6de3f51f5d887049240acd22a4fc
BabitzdaBrave <amg.vintila@gmail.com>
2018-03-29 13:43:46 -0400

    finishing up question 4

---
+++ AMTH250_NumericalMatlab/ASSIGNMENTS/Assignment 1/MonteCarloPI.m
@@ -0,0 +1,96 @@
+% Question 4: Monte Carlo
+
+% This script uses the Monte Carlo Method to get 
+% an estimate of the irration Pi. 
+
+% First
+close all; % close all figures/plots
+clf; % clears the current figure/plot - if we do close all this is not
+% necessary. 
+clear all; % clear all variables in the base workspace so the script doesn't borrow  from it. 
+clc; % clear command window. 
+
+
+% Step 1: prompt user for an integer n 
+% (must be the n = large number of random points to be
+% uniformly generated)
+n = input('Enter a large number n: ');
+while ~isnumeric(n) || ~isscalar(n) || n <= 0 || ~isfinite(n) || isnan(n) || iskeyword(n) 
+    disp('Invalid: input must be a positive integer n.')
+    n = input('Enter again: ');
+end
+
+
+% Step 2: generate the n random x-coordinates on (-1,1) and the
+% n random y coordinates on (-1, 1)
+xs = rand(1, n) * (1 - - 1) - 1;
+ys = rand(1, n) * (1 - - 1) - 1;
+
+
+% Step 3: determine the number (c) for which x^2 + y^2 <= 1
+% (meaning, the number of coordinates that lie inside or on
+% the unit circle at the origin)
+c = 0;
+
+for ii = 1:n
+    % for each coordinate pair that lies on or inside
+    % the circle, increment the count. 
+    if xs(ii)^2 + ys(ii)^2 <= 1
+        c = c + 1;
+    end
+end
+
+
+% Step 4: now estimate Pi by multiplying c/n by area of square (4)
+% that the circle lies inside
+estimatePI = 4*c / n; 
+
+format long
+disp(['The estimate of pi = ', num2str(estimatePI)])
+
+
+
+% Step 5: visualizing the calculation: 
+
+% getting points that lie outside the circle versus inside and on the
+% circle
+xsInside = zeros(1, c); % is a vector to hold x-coordinate of coordinate
+% pair that lies inside the unit circle
+ysInside = zeros(1, c);
+
+xsOutside = zeros(1, n - c);
+ysOutside = zeros(1, n- c); 
+
+for ii = 1:n
+    %  
+    if xs(ii)^2 + ys(ii)^2 <= 1
+        xsInside(ii) = xs(ii);
+        ysInside(ii) = ys(ii);
+    else % the points are outside the circle, put them in the 'outside' matrix
+        xsOutside(ii) = xs(ii);
+        ysOutside(ii) = ys(ii);
+    end
+end
+
+%figure(1)
+
+
+% plot the square with solid black lines
+plot([-1 -1, -1 1], 'k-') % at x = -1, straight vertical line from -1 to 1
+hold on
+grid on; grid minor
+plot([1 1, -1 1], 'k-') % at x = 1, straight vertical line from -1 to 1
+plot([-1 1, -1 -1], 'k-') % the bottom part of the square
+plot([-1 1, 1 1], 'k-') % the top part of the square. 
+
+axis([-1 1, -1 1]) % setting xlimits [-1, 1] and ylimits [-1, 1]
+
+% plot the unit circle with a solid black curve
+theta = 0:0.01: 2*pi; %the domain of the unit circle [0, 2pi]
+plot(cos(theta), sin(theta), 'k-'); % plotting the circle
+
+% Plotting the Monte Carlo Data. 
+plot(xsInside, ysInside, 'r.') % plot inside dots as red points. 
+plot(xsOutside, ysOutside, 'b.') % plot outside dots as blue dots. 
+
+
