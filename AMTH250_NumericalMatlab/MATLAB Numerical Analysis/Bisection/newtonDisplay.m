089928bf022f6dd2fa1e062d249cee988118c1f7
BabitzdaBrave <amg.vintila@gmail.com>
2018-03-30 14:25:52 -0400

    done newton and bisect scripts from gilat, now got to do fixed point and secant method scripts

---
+++ AMTH250_NumericalMatlab/MATLAB Numerical Analysis/Bisection/newtonDisplay.m
@@ -0,0 +1,54 @@
+
+% Gilat book, page 84 in pdf
+
+% Newton's Method (Newton Raphson) for finding roots
+
+% Requirements: 
+% (1) f(x) is continuous and differentiable (once)
+% (2) f(x) has a solution near a given point. 
+
+% FORMULA: 
+% x(i+1) = x(i) - f(x(i)) / f ' (x(i))
+
+
+% f = function
+% fDeriv = derivative function (of f)
+% xInitial = initial estimate of solution
+% tol = tolerance
+% maxIterations
+% xSol = solution
+function xSol = newtonDisplay(f, fDeriv, xInitial, tol, maxIterations)
+
+    xEst = xInitial; 
+    
+    fprintf('\niteration   xi (next)   x (est or final sol)      itol        tol\n');
+    
+    for i = 1: maxIterations
+        xi = xEst - f(xEst) / fDeriv(xEst);
+        
+        iTol = abs(xi - xEst)/xEst;
+        
+        fprintf('%3i      %11.6f       %11.6f      %11.6f  %11.6f\n', i, xi, xEst, iTol, tol);
+        
+        if iTol < tol
+            xSol = xi; % if we are below tolerance, then we are DONE!
+            break
+        end
+        % else we continue by setting the xest as previous xi
+        xEst = xi;
+    end
+    
+    if i == maxIterations
+        fprintf('Solution not obtained in %i iterations.\n', maxIterations)
+        xSol = ('No Answer');
+    end
+    
+end
+
+
+%%% STOP:
+% *when exact solutoin f(x) = 0 is found
+% * or by error bounding: 
+% (1) estimated relative error is smaller than tolerance.  epsilon
+% **** estimated relative error: |(xi+1 - xi) / xi| <= epsilon(error)
+% (2) when tolerance in f(x):   |f(xi)| <= delta
---
+++ AMTH250_NumericalMatlab/MATLAB Numerical Analysis/BookSauer/Chapter1_SolvingEquations_Roots/bisect.m
@@ -0,0 +1,38 @@
+% Sauer book, page28
+
+% Program 1.1: Bisection Method
+% Computing approximate solution of f(x) = 0
+% Input: function handle f; a,b such that f(a)*f(b) <0; and tolerance tol
+% Output: Apprxomiate solution xSol
+
+function xSol = bisect(f, a, b, tol)
+
+    if sign(f(a)) * sign(f(b)) >= 0
+        error('f(a)f(b) < 0 not satisfied!'); % stops execution
+    end
+    
+    fa = f(a);
+    fb = f(b); 
+    
+    while (b - a)/2 > tol
+        c = (a + b)/2;
+        fc = f(c);
+        
+        if fc == 0 % found it!
+            break
+        end
+        
+        if sign(fc)*sign(fa) < 0 % then new interval [a,c]
+            b = c; 
+            fb = fc; 
+        else % new interval [c, b]
+            a = c; 
+            fa = fc; 
+        end
+    end
+    
+    % when interval width is less than tolerance, %
+    % we have the solution: 
+    xSol = (a + b)/2;
+    
+end
