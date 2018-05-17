5bd3005d6088b88bb6ed8a43a42f7229ac723b21
BabitzdaBrave <amg.vintila@gmail.com>
2018-05-17 14:10:15 -0400

    done lectures 16 and 17

--- AMTH250_NumericalMatlab/Numerical Analysis/Interpolation/calcSpline.m
+++ AMTH250_NumericalMatlab/Numerical Analysis/Interpolation/calcSpline.m
@@ -1,18 +1,18 @@
 % given the list of splines and original xs, this function calculates the
 % interpolated value of the spline, depending on where it falls in the
 % interval. 
-function yInterps = calcSpline(xs, splines, xInterps)
-    n = length(xs); % test condition: length(splines) + 1 = length(xs)
-    xInterps = sym(xInterps);
+function yInterps = calcSpline(xData, splines, xInterps)
+    n = length(xData); % test condition: length(splines) + 1 = length(xs)
+    %xInterps = sym(xInterps);
     yInterps = sym(zeros(1, length(xInterps)));
-    xs = sym(xs);
+    %xData = sym(xData);
+    
+    syms x;
     
     for c = 1:length(xInterps)
-        
         for i = 1:n-1
-            if xInterps(c) >= xs(i) && xInterps(c) <= xs(i+1)
-                syms x;
-                yInterps(c) = subs(splines(i), x, xInterps(c));
+            if xInterps(c) >= xData(i) && xInterps(c) <= xData(i+1)
+                yInterps(c) = sym(subs(splines(i), x, xInterps(c)));
             end
         end
     end
